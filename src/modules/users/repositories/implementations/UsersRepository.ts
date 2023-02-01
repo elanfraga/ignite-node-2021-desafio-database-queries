f0ce68d91ff0732635c0dad48a22a10fa91b57c7
Elan Fraga <elanfraga@gmail.com>
2023-01-31 22:00:29 -0300

    fim do desafio

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -10,21 +10,39 @@ export class UsersRepository implements IUsersRepository {
   constructor() {
     this.repository = getRepository(User);
   }
+  // findAllUsersOrderedByFirstName(): Promise<User[]> {
+  //   throw new Error('Method not implemented.');
+  // }
+  // findUserByFullName(data: IFindUserByFullNameDTO): Promise<User[] | undefined> {
+  //   throw new Error('Method not implemented.');
+  // }
 
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOneOrFail({
+      relations: ['games'],
+      where: {
+        id: user_id
+      }
+    });
+
+    return user;
   }
 
+
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('select * from users order by first_name asc'); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('select * from users where upper(first_name) = upper($1) and upper(last_name) = upper($2)', 
+    [first_name, last_name]); // Complete usando raw query
   }
 }
+
+  
+
